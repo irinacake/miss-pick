e7b72ce893a1a1656f024c8620214c1f50665b91
Elana Courtines <chabigesi@gmail.com>
2024-03-15 13:45:02 +0100

    tuto otawa ex2.4 done

--- tutoriel/display/display.cpp
+++ tutoriel/display/display.cpp
@@ -25,100 +25,121 @@ protected:
     require(DECODED_TEXT);
     //require(COLLECTED_CFG_FEATURE);
 
-    // open (overwrite) 2 files
-    // part 1 : nodes
+    // open (overwrite) main output file
     auto p1 = io::FileOutput("p1.dot");
-    // part 2 : paths
-    auto p2 = io::FileOutput("p2.dot");
-
-    // basic initialisation
-    p1 << "digraph \"main\" {" << io::endl;
-    p1 << "\tnode [ shape=box ];\n" << io::endl;
-    p2 << io::endl;
 
     // get the first instruction
     auto inst = workspace()->process()->findInstAt(entry);
 
-    // declare a todo vector
+    // declare todo vectors
+    // for each functions called
+    Vector<Inst *> fnctodo;
+    // for each inst within a function
     Vector<Inst *> todo;
     // add the first int to it
-    todo.add(inst);
+    fnctodo.add(inst);
 
-    while (!todo.isEmpty() && i < 1000){
-      // pop the next inst in todo
-      auto curInst = todo.pop();
+    while (!fnctodo.isEmpty()){
+      // transfer from fnctodo to todo
+      todo.add(fnctodo.pop());
+      
+      // open (overwrite) temporary output file for paths
+      auto p2 = io::FileOutput("p2.dot");
 
-      // only work on it if not already marked
-      if (!MARK(curInst)){
-        // work to do varies based on inst type
-        if ( curInst->isReturn() ){
-          // add nothing in todo
-          // add no path
+      // basic var to give a different name
+      i2++;
 
-          // debug
-          i++;cout<<"[ret:"<<i<<"]("<<curInst->address()<<") "<<curInst<<io::endl;;
-        } else if (curInst->isBranch()) {
-          // add both next and branch
-          // todo : if "always", then skip adding nextinst
-          todo.add(curInst->target());
-          todo.add(curInst->nextInst());
+      // init the function in .dot
+      p1 << "digraph \"func" << i2 << "\" {" << io::endl;
+      p1 << "\tnode [ shape=box ];\n" << io::endl;
+      p2 << io::endl;
 
-          // add path to target
-          p2 << "\t\"" << curInst->address() << "\" -> \"" << curInst->target()->address() << "\" [ label = \"taken\" ];" << io::endl;
+      //debug
+      i = 0;
+      
+      while (!todo.isEmpty()){
+        // pop the next inst in todo
+        auto curInst = todo.pop();
 
-          //add path to next
-          p2 << "\t\"" << curInst->address() << "\" -> \"" << curInst->nextInst()->address() << "\";" << io::endl;
+        // only work on the inst if not already marked
+        if (!MARK(curInst)){
+          // work to do varies based on inst type
+          if ( curInst->isReturn() ){
+            // add nothing to todo
+            // add no path
 
-          // debug
-          i++;cout<<"[brch:"<<i<<"]("<<curInst->address()<<") "<<curInst<<io::endl;;
-        } else if (curInst->isCall()) {
-          // add both next and branch
-          // todo : if "always", then skip adding nextinst
-          todo.add(curInst->target());
-          todo.add(curInst->nextInst());
+            // debug
+            cout<<"[ret:"<<i<<"]("<<curInst->address()<<") "<<curInst<<io::endl;i++;
+          } else if (curInst->isBranch()) {
+            // add branch target to todo
+            todo.add(curInst->target());
 
-          // add path to target (call)
-          p2 << "\t\"" << curInst->address() << "\" -> \"" << curInst->target()->address() << "\" [ label = \"call\" ];" << io::endl;
-          //simplified
-          //p2 << "\t\"" << curInst->address() << "\" -> \"func\" [ label = \"call\" ];" << io::endl;
+            // add path to target
+            p2 << "\t\"" << curInst->address() << "\" -> \"" << curInst->target()->address() << "\" [ label = \"taken\" ];" << io::endl;
+            
+            // if and only if the branch is conditionnal
+            if (curInst->isCond()) {
+              // add nextinst to todo
+              todo.add(curInst->nextInst());
 
-          // add path to next
-          p2 << "\t\"" << curInst->address() << "\" -> \"" << curInst->nextInst()->address() << "\";" << io::endl;
+              // add path to next
+              p2 << "\t\"" << curInst->address() << "\" -> \"" << curInst->nextInst()->address() << "\";" << io::endl;
+            }
+            // else, the nextInst will never be taken
 
-          // debug
-          i++;cout<<"[call:"<<i<<"]("<<curInst->address()<<") "<<curInst<<io::endl;;
+            // debug
+            cout<<"[brch:"<<i<<"]("<<curInst->address()<<") "<<curInst<<io::endl;i++;
+          } else if (curInst->isCall()) {
+            // add both next and call to todo
+            fnctodo.add(curInst->target());
+            todo.add(curInst->nextInst());
+
+            // add path to target (call)
+            //p2 << "\t\"" << curInst->address() << "\" -> \"" << curInst->target()->address() << "\" [ label = \"call\" ];" << io::endl;
+            //simplified
+            p2 << "\t\"" << curInst->address() << "\" -> \"func\" [ label = \"call\" ];" << io::endl;
+
+            // add path to next
+            p2 << "\t\"" << curInst->address() << "\" -> \"" << curInst->nextInst()->address() << "\";" << io::endl;
+
+            // debug
+            cout<<"[call:"<<i<<"]("<<curInst->address()<<") "<<curInst<<io::endl;i++;
+          }
+          
+          else {
+            // default, add next to todo
+            todo.add(curInst->nextInst());
+
+            // add path to next
+            p2 << "\t\"" << curInst->address() << "\" -> \"" << curInst->nextInst()->address() << "\";" << io::endl;
+
+            // debug
+            cout<<"[:"<<i<<"]("<<curInst->address()<<") "<<curInst<<io::endl;i++;
+          }
+
+          // append node to the .dot in every case
+          p1 << "\t\"" << curInst->address() << "\" [ label = \"" << curInst->address() << " " << curInst << "\" ];" << io::endl;
+
+          // mark current as done
+          MARK(curInst) = true;
         }
-        
-        else {
-          // default, add next
-          todo.add(curInst->nextInst());
-
-          // add path to next
-          p2 << "\t\"" << curInst->address() << "\" -> \"" << curInst->nextInst()->address() << "\";" << io::endl;
-
-          // debug
-          i++;cout<<"[:"<<i<<"]("<<curInst->address()<<") "<<curInst<<io::endl;;
-        }
-
-        // append node to the .dot in every case
-        p1 << "\t\"" << curInst->address() << "\" [ label = \"" << curInst->address() << " " << curInst << "\" ];" << io::endl;
-
-        // mark current as done
-        MARK(curInst) = true;
-
       }
+
+      // read the temporary file as input
+      auto p2b = io::FileInput("p2.dot");
+      // append its lines to the main file
+      for(auto line: p2b.lines()){
+        p1 << line;
+      }
+      p1 << "}\n" << io::endl;
+      // this simply allows to split the nodes and the paths
     }
 
-
-    auto p2b = io::FileInput("p2.dot");
-    for(auto line: p2b.lines()){
-      p1 << line;
-    }
-    p1 << "}" << io::endl;
   }
 
 private:
   int i=0;
+  int i2=0;
 };
 
 
