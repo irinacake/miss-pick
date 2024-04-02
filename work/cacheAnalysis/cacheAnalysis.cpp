bf5a602f5760e50ee37f1d0aeeca229ac7a93a38
Elana Courtines <chabigesi@gmail.com>
2024-04-02 10:41:12 +0200

    add comments to State and SaveState classes

--- work/cacheAnalysis/cacheAnalysis.cpp
+++ work/cacheAnalysis/cacheAnalysis.cpp
@@ -7,37 +7,63 @@
 #include <otawa/hard/features.h>
 #include <otawa/hard/CacheConfiguration.h>
 
-
 #include <elm/assert.h>
 #include <elm/sys/FileItem.h>
 #include <elm/util/BitVector.h>
-
 
 using namespace elm;
 using namespace otawa;
 
 
+/**
+ * @fn
+ * 
+ * @param
+ * @return  
+ */
 
-
+/**
+ * @class State
+ * A simplistic representation of the state of a cache set
+ * 
+ * @param isize the size of the cache set to initialise
+ */
 class State {
   public:
   State(int isize): size(isize) {
     state = new otawa::hard::Cache::block_t[size];
   }
+
+  /**
+   * @fn getState
+   * @return otawa::hard::Cache::block_t* state
+   */
   inline otawa::hard::Cache::block_t* getState(){
     return state;
   }
+
+  /**
+   * @fn setValue
+   * Sets the value of an element of the state table.
+   * @param pos int
+   * @param value otawa::hard::Cache::block_t (elm::t::uint32)
+   */
   inline void setValue(int pos, otawa::hard::Cache::block_t value){
+    ASSERTP(pos >= 0 && pos < size, "In State.setValue() : argument 'pos', index out of bound.");
     state[pos] = value;
   }
-  friend elm::io::Output &operator<<(elm::io::Output &output, const State &state);
 
+  /**
+   * @fn equals
+   * Tests the equality between this state and a given state
+   * 
+   * @param state2 the state to compare to
+   * @return true if both states are identical, false otherwise
+   */
   bool equals(State* state2){
     if (this->size == state2->size) {
       for (int i = 0; i < this->size; i++) {
-        if (this->state[i] != state2->state[i] ) {
-          return false;
-        }
+        if (this->state[i] != state2->state[i]) return false;
       }
       return true;
     } else {
@@ -45,11 +71,14 @@ class State {
     }
   }
 
+  friend elm::io::Output &operator<<(elm::io::Output &output, const State &state);
+  
   private:
   int size;
   otawa::hard::Cache::block_t* state;
 };
 
+// Redefinition of the << operator for the State class
 elm::io::Output &operator<<(elm::io::Output &output, const State &state) {
   output << "( ";
   for (int i = 0; i < state.size ; i++){
@@ -59,20 +88,38 @@ elm::io::Output &operator<<(elm::io::Output &output, const State &state) {
   return output;
 }
 
-
+// Redefinition of the equivalence between two State* for the
+// List.contains() method
 namespace elm {
-template<>
-class Equiv<State *> {
-public:
-  static inline bool isEqual(State *state1, State *state2) {
-    return state1->equals(state2);
-  }
-};
-
+  template<>
+  class Equiv<State *> {
+  public:
+    static inline bool isEqual(State *state1, State *state2) {
+      return state1->equals(state2);
+    }
+  };
 }
 
 
 
+
+/**
+ * @class SaveState
+ * A SaveState is a data structure that store multiple cache states
+ * for multiple cache sets. It uses the State class as well as the
+ * elm List structure to store cache states.
+ * 
+ * The default constructor does not initialise the cache structure.
+ * To initiliase it, first create a class, then call the setCache()
+ * method :
+ * @code
+ * SaveState* newSaveState = new SaveState;
+ * newSaveState->setCache(size);
+ * @endcode
+ * 
+ * The class was implemented this way to allow the use of properties.
+ * 
+ */
 class SaveState {
   public:
   SaveState(): saved(nullptr), size(0) {}
@@ -85,12 +132,25 @@ class SaveState {
       listSizes[i] = 0;
     }
   }
+  void setCache(int setCount){
+    size = setCount;
+    saved = new List<State *> [size];
+    listSizes = new int[size];
+    for (int i = 0; i < size; i++){
+      listSizes[i] = 0;
+    }
+  }
 
   void add(State *newState, int set) {
+    ASSERTP(set >= 0 && set < size, "In SaveState.add() : argument 'set', index out of bound.");
     if (!(saved[set].contains(newState))){
       listSizes[set]++;
       saved[set].add(newState);
     }
+  }
+
+  int* getListSizes(){
+    return listSizes;
   }
 
   friend elm::io::Output &operator<<(elm::io::Output &output, const SaveState &saveState);
@@ -116,22 +176,22 @@ elm::io::Output &operator<<(elm::io::Output &output, const SaveState &saveState)
 
 
 
+/**
+ * @class CacheState
+ * A CacheState is an encapsulation of the otawa::hard::Cache class.
+ * It stores the current cache state using an AllocArray and provides
+ * various update methods based on the update policy.
+ * 
+ * @param icache the otawa::hard::Cache object to encapsulate.
+ */
 class CacheState {
 public:
   CacheState(const otawa::hard::Cache* icache): cache(icache), nbSets(icache->setCount()), nbWays((int)pow(2,icache->wayBits())) { 
-  
     state.allocate(nbSets * nbWays);
-
-    
-
     for (int e=0; e < (nbSets * nbWays); e++) {
       state[e] = otawa::hard::Cache::block_t(-1);
     } 
- 
-    displayState();
   }
-
-  ~CacheState(){ }
 
   void updateLRU(otawa::address_t toAdd){
     auto toAddSet = cache->set(toAdd);
@@ -188,7 +248,6 @@ public:
   }
 
   State* getSubState(otawa::address_t toGet){
-    //todo : make a "new" item
     auto toGetSet = cache->set(toGet);
     State* newState = new State(nbWays);
     int pos = 0;
@@ -203,9 +262,9 @@ private:
   int nbWays;
   int nbSets;
   AllocArray<otawa::hard::Cache::block_t> state;
-  //otawa::hard::Cache::block_t* state;
   const otawa::hard::Cache* cache;
 };
+
 
 
 
@@ -213,92 +272,115 @@ p::id<SaveState*> SAVED("SAVED");
 
 
 
-void printStates(CFG *g, CacheState *mycache,
-              int currTag = -1, int currSet = -1, string indent = "") {
-
+void printStates(CFG *g, CacheState *mycache, string indent = "") {
   for(auto v: *g){
 		if(v->isSynth()) {
-			printStates(v->toSynth()->callee(), mycache, currTag, currSet, indent + "\t");
+			printStates(v->toSynth()->callee(), mycache, indent + "\t");
     } else if (v->isBasic()) {
       cout << indent << **SAVED(v) << endl;
-      
-      /*
-      for (auto inst : *v->toBasic()){
-        if (currTag != mycache->getTag(inst->address())
-            || currSet != mycache->getSet(inst->address()) ){
-          currTag = mycache->getTag(inst->address());
-          currSet = mycache->getSet(inst->address());
-
-        }
-      }
-      */
     }
   }
 }
 
-void initState(CFG *g, CacheState *mycache,
-              int currTag = -1, int currSet = -1, string indent = "") {
+void initState(CFG *g, CacheState *mycache, string indent = "") {
   for(auto v: *g){
 		if(v->isSynth()) {
-			initState(v->toSynth()->callee(), mycache, currTag, currSet, indent + "\t");
+			initState(v->toSynth()->callee(), mycache, indent + "\t");
     } else if (v->isBasic()) {
-      
       SaveState* newSaveState = new SaveState;
-      
       newSaveState->setCache(mycache->getCache());
-      
       SAVED(v) = newSaveState;
-      /*
-      for (auto inst : *v->toBasic()){
-        if (currTag != mycache->getTag(inst->address())
-            || currSet != mycache->getSet(inst->address()) ){
-          currTag = mycache->getTag(inst->address());
-          currSet = mycache->getSet(inst->address());
-
-        }
-      }*/
     }
   }
 }
 
-void statetest(CFG *g, CacheState *mycache,
-              int currTag = -1, int currSet = -1, string indent = "") {
+void statetest(CFG *g, CacheState *mycache, string indent = "") {
 
+  int currTag;
+  int currSet;
 	for(auto v: *g){
 		if(v->isSynth()) {
-			statetest(v->toSynth()->callee(), mycache, currTag, currSet, indent + "\t");
+			statetest(v->toSynth()->callee(), mycache, indent + "\t");
     } else if (v->isBasic()) {
-
       currTag = -1;
       currSet = -1;
-
       for (auto inst : *v->toBasic()){
-
-        //cout << indent << "inst adr : " << inst->address() << endl;
-        //cout << indent << "inst tag : " << mycache->getTag(inst->address()) << endl;
-        //cout << indent << "inst set : " << mycache->getSet(inst->address()) << endl;
-
         if (currTag != mycache->getTag(inst->address())
             || currSet != mycache->getSet(inst->address()) ){
-          mycache->updateLRU(inst->address());
 
           currTag = mycache->getTag(inst->address());
           currSet = mycache->getSet(inst->address());
 
           State* newState = mycache->getSubState(inst->address());
-          //cout << indent << "current states : " << **SAVED(v) << endl;
-          //cout << indent << "new state : " << *newState << endl;
-
           SAVED(v)->add(newState, currSet);
 
-        } else {
-          //cout << indent << "-> cache hit!" << endl;
+          mycache->updateLRU(inst->address());
+
         }
-        //cout << endl << endl;
       }
     }
   }
 }
+
+
+
+void getStats(CFG *g, int *mins, int *maxs, float *moys, int* bbCount, int waysCount) {
+  for(auto v: *g){
+		if(v->isSynth()) {
+			getStats(v->toSynth()->callee(), mins, maxs, moys, bbCount, waysCount);
+    } else if (v->isBasic()) {
+      SaveState* sState = *SAVED(v);
+      int* listSizes = sState->getListSizes();
+      for (int i = 0; i < waysCount; i++){
+        mins[i] = min(mins[i],listSizes[i]);
+        maxs[i] = max(maxs[i],listSizes[i]);
+        moys[i] += listSizes[i];
+        (*bbCount)++;
+      }
+    }
+  }
+}
+
+void makeStats(CFG *g, CacheState *mycache) {
+  int waysCount = mycache->getCache()->setCount();
+  int mins[waysCount];
+  int maxs[waysCount];
+  float moys[waysCount];
+  for (int i = 0; i < waysCount; i++){
+    mins[i] = type_info<int>::max;
+    maxs[i] = 0;
+    moys[i] = 0;
+  }
+  int bbCount = 0;
+
+
+  getStats(g, mins, maxs, moys, &bbCount, waysCount);
+
+  cout << "bbcount : " << bbCount << endl;
+
+  cout << "mins : ";
+  for (int i = 0; i < waysCount; i++){
+    cout << mins[i] << " ";
+  }
+  cout << endl;
+
+  cout << "maxs : ";
+  for (int i = 0; i < waysCount; i++){
+    cout << maxs[i] << " ";
+  }
+  cout << endl;
+
+  cout << "moys : ";
+  for (int i = 0; i < waysCount; i++){
+    moys[i] /= bbCount;
+    cout << moys[i] << " ";
+  }
+  cout << endl;
+
+}
+
+
+
 
 
 class CacheAnalysis: public Application {
@@ -311,7 +393,6 @@ protected:
     //otawa::VERBOSE(props) = true;
     otawa::CACHE_CONFIG_PATH(props) = "mycache.xml";
 
-
     require(DECODED_TEXT);
     require(COLLECTED_CFG_FEATURE);
     require(otawa::hard::CACHE_CONFIGURATION_FEATURE);
@@ -323,11 +404,11 @@ protected:
     auto icache = confs->instCache();
     CacheState mycache(icache);
 
-    
-
     initState(maincfg, &mycache);
     statetest(maincfg, &mycache);
-    printStates(maincfg, &mycache);
+    //printStates(maincfg, &mycache);
+
+    makeStats(maincfg, &mycache);
     
   }
 
